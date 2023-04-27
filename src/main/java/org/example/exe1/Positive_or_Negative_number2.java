cb2b268641ae90dadbc468b24b595ac8f8e40bae
Officialravi888 <youremail@yourdomain.com>
2023-04-27 14:40:15 +0530

    Positive_or_Negative_number2

---
+++ src/main/java/org/example/exe1/Positive_or_Negative_number2.java
@@ -0,0 +1,14 @@
+package org.example.exe1;
+
+public class Positive_or_Negative_number2 {
+    public static void main(String[] args) {
+        int num=0;
+        if(num==0){
+            System.out.println("Zero");
+        }
+        else{
+            String result=num >0 ?"positive":"nigitive";
+                System.out.println(result);
+        }
+    }
+}
