89dee799b48f6b9068a5495399efa16aff3ec0e7
Officialravi888 <youremail@yourdomain.com>
2023-04-27 14:28:54 +0530

    Positive_or_Negative_number

---
+++ src/main/java/org/example/exe1/Positive_or_Negative_number.java
@@ -0,0 +1,13 @@
+package org.example.exe1;
+
+public class Positive_or_Negative_number {
+    public static void main(String[] args) {
+        int a=11;
+        if(a>0)
+            System.out.println("Number is positive");
+        else if (a<0)
+            System.out.println("Number is Nagtive");
+        else
+            System.out.println("Zero");
+        }
+    }
