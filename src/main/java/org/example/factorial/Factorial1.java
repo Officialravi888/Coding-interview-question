4a97fa943310d4867424e55906ffc11ff01eb048
Officialravi888 <youremail@yourdomain.com>
2023-04-29 17:48:54 +0530

    Factorial1

--- src/main/java/org/example/factorial/Factorial1.java
+++ src/main/java/org/example/factorial/Factorial1.java
@@ -1,4 +1,15 @@
 package org.example.factorial;
 
 public class Factorial1 {
+    public static void main(String[] args) {
+        int num=6;
+        System.out.println(""+num+"="+fact(num));
+    }
+    static int fact(int num){
+        int res=1;
+        for(int i=2; i<num; i++){
+            res*=i;
+        }
+        return res;
+    }
 }
